d8f1e51d203264484cfde6e57186e5298980b984
ZafarKamal123 <sk4915497@gmail.com>
2022-06-29 14:09:17 +0500

    :wink: finalize the extension

--- src/index.js
+++ src/index.js
@@ -8,4 +8,52 @@ import './editor.scss';
  */
 import './style.scss';
 
-// Your code goes here...
+import { __ } from '@wordpress/i18n';
+import { addFilter } from '@wordpress/hooks';
+import { Fragment } from '@wordpress/element';
+import { BlockControls } from '@wordpress/block-editor';
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { ToolbarGroup, ToolbarButton } from '@wordpress/components';
+
+import shapes from './shapes.json';
+
+ 
+const withInspectorControls = createHigherOrderComponent( ( BlockEdit ) => {
+    return ( props ) => {
+
+        if ( props.name !== "core/cover" ) {
+            return <BlockEdit {...props} />
+        }
+
+        const updateBackgroundShape = () => {
+
+            const newRandomShape = shapes[Math.floor(Math.random() * shapes.length)];
+            const encodedShape = `data:image/svg+xml,` + encodeURIComponent(newRandomShape);
+
+            props.setAttributes({ url: encodedShape });
+
+        }
+
+        return (
+            <Fragment>
+                <BlockEdit { ...props } />
+                <BlockControls>
+                    <ToolbarGroup>
+                        <ToolbarButton 
+                            showTooltip
+                            icon="update"
+                            onClick={updateBackgroundShape}
+                            label={__('Update shape', 'gutenberghub-random-background')}
+                        />
+                    </ToolbarGroup>
+                </BlockControls>
+            </Fragment>
+        );
+    };
+}, 'withInspectorControl' );
+ 
+addFilter(
+    'editor.BlockEdit',
+    'my-plugin/with-inspector-controls',
+    withInspectorControls
+);
